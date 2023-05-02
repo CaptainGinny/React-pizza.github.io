3f3f79178a2c98f5e38e9124255459df65425e99
CaptainGinny <ginnymalko@gmail.com>
2023-05-02 18:15:27 +0300

    Add Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
+
 import Header from './сomponents/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
@@ -13,10 +16,20 @@ export const SearchContext = React.createContext('');
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
 
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="wrapper">
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
