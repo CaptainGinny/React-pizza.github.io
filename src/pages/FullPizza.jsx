feffbd134f768372a2a1037858e30af47906d8f0
CaptainGinny <ginnymalko@gmail.com>
2023-06-22 12:16:24 +0300

    ADD useLocation

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function FullPizza() {
+  return (
+    <div className="container">
+      <img src="" />
+      <h2>2222</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat quidem fugit saepe,
+        voluptatibus soluta minima.
+        <h4>250 p.</h4>
+      </p>
+    </div>
+  );
+}
