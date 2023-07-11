b1f1c047afa500d7b577acb6266c1a60f0d0d28a
CaptainGinny <ginnymalko@gmail.com>
2023-07-11 21:13:46 +0300

    index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
-import App from './App.tsx'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-);
+const rootElem = document.getElementById('root');
+
+if(rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+  root.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+  );
+}
