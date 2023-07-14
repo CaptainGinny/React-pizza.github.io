b8b12266c5b1a1da05cfb7b91a318fe5b368cbfa
CaptainGinny <ginnymalko@gmail.com>
2023-07-14 15:43:46 +0300

    Type Redux

---
+++ src/redux/slices/pizzaSlice.ts
@@ -0,0 +1,66 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { RootState } from '../store';
+
+type Pizza = {
+  id: string;
+  title: string;
+  price: number;
+  imageUrl: string;
+  sizes: number[];
+  types: string[];
+  rating: number;
+};
+
+interface PizzaSliceState {
+  items: Pizza[];
+  status: 'loading' | 'success' | 'error';
+}
+
+const initialState: PizzaSliceState = {
+  items: [],
+  status: 'loading', // lading | success | error
+};
+
+export const fetchPizzas = createAsyncThunk(
+  'pizza/fetchPizzasStatus',
+  async (params: Record<string, string>) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    const { data } = await axios.get<Pizza[]>(
+      `https://6419b14fc152063412c93ba7.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+    );
+    return data as Pizza[];
+  },
+);
+
+const pizzaSlice = createSlice({
+  name: 'pizza',
+  initialState,
+  reducers: {
+    setItems(state, action: PayloadAction<Pizza[]>) {
+      state.items = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchPizzas.pending, (state, action) => {
+      state.status = 'loading';
+      state.items = [];
+    });
+
+    builder.addCase(fetchPizzas.fulfilled, (state, action) => {
+      state.items = action.payload;
+      state.status = 'success';
+    });
+
+    builder.addCase(fetchPizzas.rejected, (state, action) => {
+      state.status = 'error';
+      state.items = [];
+    });
+  }
+});
+
+export const selectPizzaData = (state: RootState) => state.pizza;
+
+export const { setItems } = pizzaSlice.actions;
+export default pizzaSlice.reducer;
--- src/сomponents/PizzaBlock/index.tsx
+++ src/сomponents/PizzaBlock/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { addItem, selectCartItemById } from '../../redux/slices/cartSlice';
+import { CartItem, addItem, selectCartItemById } from '../../redux/slices/cartSlice';
 
 type PizzaBlockProps = { 
   id: string; 
@@ -24,13 +24,14 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({ id, title, price, imageUrl, siz
   const addedCount = cartItem ? cartItem.count : 0;
 
   const onClickAdd = () => {
-    const item = {
+    const item: CartItem = {
       id,
       title,
       price,
       imageUrl,
       type: typeNames[activeType],
       sizes: sizes[activeSize],
+      count: 0,  // FIX
     };
     dispatch(addItem(item));
   };
