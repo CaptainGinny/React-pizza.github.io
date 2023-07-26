5cea214b8a96db3f25c8eb2fca3124fd8a1835ad
CaptainGinny <ginnymalko@gmail.com>
2023-07-26 12:21:12 +0300

    Loadable

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import Home from './pages/Home';
 import './scss/app.scss';
 import MainLayout from './layouts/MainLayout';
 
+//Ленивая подгрузка на стороне сервера
 const Cart = Loadable({
   loader: () => import(/* webpackChunkName: "Cart" */ './pages/Cart'),
   loading: () => <div>Идёт загрузка корзины...</div>,
 });
 
+//Ленивая подгрузка в браузере
 const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './pages/FullPizza'));
 const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
