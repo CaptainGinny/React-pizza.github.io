adfdeabee4402fd1c4275a99329511a1e3a581c0
CaptainGinny <ginnymalko@gmail.com>
2023-08-15 19:28:02 +0300

    007

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -32,7 +32,7 @@ const FullPizza: React.FC = () => {
 
   return (
     <div className="container">
-      <img src={pizza.imageUrl} />
+      <img src={pizza.imageUrl} alt='Pizza' />
       <h2>{pizza.title}</h2>
       <h4>{pizza.price} $</h4>
       <Link to="/">
