77e3698bc334c46f7d75cbfa5bd615684c4608f3
CaptainGinny <ginnymalko@gmail.com>
2023-12-14 14:20:28 +0300

    The finished working design is not packaged

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -39,9 +39,10 @@ export const Sort: React.FC<SortPopupProps> = React.memo(({ value }) => {
     const handleClickOutside = (event: MouseEvent) => {
       const _event = event as PopupClick;
 
-      if (sortRef.current && !_event.path.includes(sortRef.current)) {
+      if (_event.path && _event.target && sortRef.current && !_event.path.includes(sortRef.current)) {
         setOpen(false);
       }
+      
     };
 
     document.body.addEventListener('click', handleClickOutside);
